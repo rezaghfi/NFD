0091f56d13296b12c3f3147e3d5b947dd6b397d5
Reza Gholamalitabar <reza.gh.fi@gmail.com>
2022-05-30 10:45:47 +0430

    1

--- daemon/cs-clustering.cpp
+++ daemon/cs-clustering.cpp
@@ -7,14 +7,16 @@ namespace nfd{
 namespace cs{
 
 void createClustring(int nodesNum, int clustersNum, int clusterHead, int clusterNode[][]){
+  //read file and create clustring
+  double fc[routerNum];
+  fc[i] = (cnorm*(1-k*ct)+pnorm*(1-k*pt)+ramnorm*(1-(k*ramt)))/((1-k*ct)+(1-kpt)+(1-k*ramt));
   
 }
 int routerSelect(int routerNum){
   int temp;
   double p[routerNum];
-  double fc[routerNum];
+  
   for(int i=0; i<routerNum;i++){
-    fc[i] = (cnorm*(1-k*ct)+pnorm*(1-k*pt)+ramnorm*(1-(k*ramt)))/((1-k*ct)+(1-kpt)+(1-k*ramt));
     p[i] = nnorm * (1-k*nt) + hnorm * (1-k*ht) _cnorm*(1-k*ct)+fnorm*(1-k*ft)
     if(p[i]>p[i+1])
       temp = i
